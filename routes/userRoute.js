ba6637e809fe250bba7ad72b27db6d9d45b91aae
saeed <saeedelnahas36@gmail.com>
2022-06-05 19:56:31 +0200

    first commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -16,7 +16,7 @@ router.post('/login', login);
 router.get('/', getAllUsers);
 router.get('/:id', getSingleUser);
 router.patch('/updateprofile', auth, updateProfile);
-router.patch('/:id', updatePassword);
+router.patch('/updatepassword/:id',  updatePassword);
 router.delete('/:id', deleteUser);
 
 module.exports = router;
